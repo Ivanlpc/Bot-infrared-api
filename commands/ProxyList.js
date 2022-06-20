7bcaad3f803ab7ffbf7953c84a9c71b5f99ec8c8
Ivanlpc <ivanlpz02@uma.es>
2022-06-20 18:03:47 +0200

    server info on proxy list

--- commands/ProxyList.js
+++ commands/ProxyList.js
@@ -3,6 +3,7 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const config = require('../config.json');
 const ExtraFunctions = require('../classes/ExtraFunctions.js');
 const API = require("../classes/API");
+const MCSrvStatus = require('../classes/MCSrvStatus.js');
 
 console.log("\u001b[32m", "[✔] Loaded Proxy-List command", "\u001b[0m");
 
@@ -16,7 +17,7 @@ module.exports = {
         ef.checkPerms(interaction.user.id).then((status) => {
 
             if (!status.hasPermission) {
-                interaction.reply({ content: config.error.noPermission, ephemeral:true });
+                interaction.reply({ content: config.error.noPermission, ephemeral: true });
             } else {
                 a = new API();
 
@@ -53,20 +54,37 @@ module.exports = {
                         try {
                             let id = SelectorInteraction.first().values[0];
                             a.getProxyInfo(id).then((info) => {
-                                
-                                let domainNames = "";
+                                let mc = new MCSrvStatus();
+                                mc.getServerInformation(info.data.domainNames[0]).then((server) => {
+                                   
+                                    let embed = new MessageEmbed()
+                                        .setTitle(config.embeds.serverinfo.title)
+                                        .setColor(config.embeds.serverinfo.color)
+                                        .setFooter({ iconUrl: config.embeds.footer.iconUrl, text: config.embeds.footer.text })
+                                        .setImage(`${config.STATUS_IMG_URL}${server.hostname}/${server.port}/banner.png`)
 
-                                info.data.domainNames.forEach((element) => {
-                                    domainNames += element + "\n";
-                                });
-                                const embed = new MessageEmbed()
-                                    .setTitle(id)
-                                    .setColor(config.embeds['proxy-list'].color)
-                                    .setFooter({ iconUrl: config.embeds.footer.iconUrl, text: config.embeds.footer.text })
-                                    .addFields(
-                                        { name: config.embeds['proxy-list'].fields.domainNames, value: domainNames }
-                                    )
-                                interaction.editReply({ embeds: [embed], components: [] })
+                                    if (server.online) {
+
+                                        embed.setThumbnail(`${config.ICON_URL}${server.hostname}`)
+                                            .setDescription(
+                                                config.embeds.serverinfo.server + ": `" + server.hostname + "`\n" +
+                                                config.embeds.serverinfo.ip + ": `" + server.ip + "`\n" +
+                                                config.embeds.serverinfo.version + ": `" + server.version + "`\n" +
+                                                config.embeds.serverinfo.online + ": `" + server.players.online + " / " + server.players.max + "`\n" +
+                                                config.embeds.serverinfo.protected.name + ": `" + (server.protected ? config.embeds.serverinfo.protected.yes : config.embeds.serverinfo.protected.no) + "`")
+                                            .setThumbnail(`${config.ICON_URL}${server.hostname}`)
+
+                                    } else {
+
+                                        embed.setThumbnail(config.embeds.serverinfo.offline_thumbnail)
+                                            .setDescription(
+                                                config.embeds.serverinfo.server + ": `" + server.hostname + "`\n" +
+                                                config.embeds.serverinfo.ip + ": `" + server.ip + "`\n" +
+                                                config.embeds.serverinfo.port + ": `" + server.port + "`");
+                                    }
+                                    interaction.editReply({ embeds: [embed], components: [] })
+                                })
+
                             }).catch((err) => {
                                 console.log(err);
                                 interaction.editReply({ content: config.error.API_error, components: [] })
